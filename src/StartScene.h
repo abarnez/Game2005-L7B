43f74cd9cef95e0556723e011db43eaef92a949b
abarnez <alexander_barnes@outlook.com>
2019-10-17 13:58:50 -0400

    adding some function, not fully working yet need to fix some stuff

--- src/StartScene.h
+++ src/StartScene.h
@@ -24,6 +24,30 @@ private:
 	Label* m_pInstructionsLabel;*/
 
 	Ship* m_pShip;
+
+	glm::vec2 m_mousePosition;
+
+	// ImGui utility functions
+	void m_ImGuiKeyMap();
+	void m_ImGuiSetStyle();
+	void m_updateUI();
+
+	// ImGui menu variables
+	bool m_exitApp = false;
+	bool m_displayAbout = false;
+	bool m_displayUI = true;
+
+
+	//Physx Variables
+	bool m_isGravityEnabled = false;
+	float m_gravity = 9.8f;
+	glm::vec2 m_position = glm::vec2(0.0f, 0.0f);
+	glm::vec2 m_velocity = glm::vec2(0.0f, 0.0f);
+	glm::vec2 m_acceleration  = glm::vec2(0.0f, 0.0f);
+	float m_PPM = 10.0f; //pixles per meter
+	float m_time = 0.016f; //time step
+	float m_Atime = 0.016f;//accel time
+	void m_move();
 };
 
 #endif /* defined (__START_SCENE__) */
